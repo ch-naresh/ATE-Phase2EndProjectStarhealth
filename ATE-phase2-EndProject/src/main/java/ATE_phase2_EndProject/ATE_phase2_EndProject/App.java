d2f8d13e2d1b6dcd7eb08420ab8bb13149d832e5
ch-naresh <148947389+ch-naresh@users.noreply.github.com>
2023-12-01 18:16:53 +0530

    Add files via upload

---
+++ ATE-phase2-EndProject/src/main/java/ATE_phase2_EndProject/ATE_phase2_EndProject/App.java
@@ -0,0 +1,13 @@
+package ATE_phase2_EndProject.ATE_phase2_EndProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
