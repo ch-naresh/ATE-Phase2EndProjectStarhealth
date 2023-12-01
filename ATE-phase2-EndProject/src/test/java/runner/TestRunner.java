66f841db89b7f67db6ca2ac76971ce5ed3e56fc4
ch-naresh <148947389+ch-naresh@users.noreply.github.com>
2023-12-01 18:40:30 +0530

    TestRunner.java

--- ATE-phase2-EndProject/src/test/java/runner/TestRunner.java
+++ ATE-phase2-EndProject/src/test/java/runner/TestRunner.java
@@ -6,7 +6,7 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="C:\\Users\\chand\\eclipse-workspace\\Selenium_Workspace\\Selenium-Basics\\src\\test\\java\\features\\StarHealth.feature",
+@CucumberOptions(features="C:\Users\ASUS\eclipse-workspace\SeleniumWorkspace\ATE-phase2-EndProject\src\test\java\feature\starHealth.feature",
 				glue= {"steps"},
 				plugin= {"html:target/Cucumberreport.html",
 						"pretty",
